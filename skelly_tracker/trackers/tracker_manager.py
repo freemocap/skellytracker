56cf1d03c6c5d578ee94eb74da9a4778c9405265
jonmatthis <jonmatthis@gmail.com>
2023-05-14 16:20:12 -0400

    uses queues an pipe now

--- skelly_tracker/trackers/tracker_manager.py
+++ skelly_tracker/trackers/tracker_manager.py
@@ -1,34 +1,43 @@
-import cv2
-import argparse
+import multiprocessing as mp
+import time
+from asyncio import sleep
 from typing import List
 
-import skelly_tracker
 from skelly_tracker.trackers.base_tracker.base_tracker import BaseTracker
 from skelly_tracker.trackers.bright_point_tracker.brightest_point_tracker import BrightestPointTracker
-from skelly_tracker.trackers.webcam_demo_viewer.webcam_demo_viewer import WebcamDemoViewer
 
 
 class TrackerManager:
     def __init__(self, trackers: List[BaseTracker]):
         self.trackers = trackers
+        self.parent_connection, self.child_connection = mp.Pipe()
+        self.process = mp.Process(target=self._process_images, args=(self.child_connection, self.trackers))
+        self.process.start()
 
-    def process_image(self, image: cv2.VideoCapture):
-        for tracker in self.trackers:
-            tracker.process_image(image)
+    @staticmethod
+    def _process_images(conn, trackers):
+        while True:
+            time.sleep(0.001)
+            image = conn.recv()
+            if image is None:
+                break
+            for tracker in trackers:
+                tracker.process_image(image)
+
+    def add_image(self, image):
+        self.parent_connection.send(image)
 
     def demo(self):
         self.trackers[0].demo()
 
+    def stop(self):
+        self.parent_connection.send(None)
+        self.process.join()
+
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Tracker Manager CLI.')
-    parser.add_argument('--trackers', nargs='+', help='List of trackers to be used.')
-    args = parser.parse_args()
 
-    # Here, we assume that each tracker name corresponds to a class in the skelly_tracker.trackers module
-    # and that each class has a no-argument constructor.
-    # You will need to adjust this if your trackers need arguments to their constructors,
-    # or if the trackers are not in the skelly_tracker.trackers module.
     trackers = [BrightestPointTracker()]
 
     manager = TrackerManager(trackers)
