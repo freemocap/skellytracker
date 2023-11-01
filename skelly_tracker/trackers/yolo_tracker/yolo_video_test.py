4b4c5585f7a963251a1c10090bff3dbec37184ed
philipqueen <queen.philip@gmail.com>
2023-11-01 11:53:47 -0600

    test video folder process

--- skelly_tracker/trackers/yolo_tracker/yolo_video_test.py
+++ skelly_tracker/trackers/yolo_tracker/yolo_video_test.py
@@ -1,6 +1,19 @@
+from pathlib import Path
+
+import numpy as np
 from skelly_tracker.trackers.yolo_tracker.yolo_tracker import YOLOPoseTracker
 
 
 if __name__ == "__main__":
-    video_path = "/Users/philipqueen/freemocap_data/recording_sessions/freemocap_sample_data/synchronized_videos/sesh_2022-09-19_16_16_50_in_class_jsm_synced_Cam2.mp4"
-    YOLOPoseTracker().process_video(video_filepath=video_path)
+    synchronized_video_path = Path("")
+    output_path = synchronized_video_path.parent / "output_data" / "raw_data" / "mediapipe2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy"
+    
+    array_list = []
+    for video_path in synchronized_video_path.glob("*.mp4"):
+        output_array = YOLOPoseTracker().process_video(video_filepath=video_path, save_data_bool=False)
+        array_list.append(output_array)
+
+    combined_array = np.stack(array_list)
+
+    print(combined_array.shape)
+    np.save(output_path, combined_array)
