5a1d4ca4ae91ade0e8bdcbaa784ff492308c0d4e
philipqueen <queen.philip@gmail.com>
2023-12-01 15:08:01 -0700

    programatically set mediapipe num tracked points

--- skelly_tracker/trackers/mediapipe_tracker/mediapipe_model_info.py
+++ skelly_tracker/trackers/mediapipe_tracker/mediapipe_model_info.py
@@ -1,9 +1,24 @@
+from mediapipe.python.solutions import holistic as mp_holistic
+from mediapipe.python.solutions.face_mesh import FACEMESH_NUM_LANDMARKS_WITH_IRISES
+
+mediapipe_body_landmark_names = [
+    landmark.name.lower() for landmark in mp_holistic.PoseLandmark
+]
+mediapipe_hand_landmark_names = [
+    landmark.name.lower() for landmark in mp_holistic.HandLandmark
+]
+
+
 class MediapipeModelInfo:
-    num_tracked_points_total = 543
-    num_tracked_points_body = 33
-    num_tracked_points_face = 468
-    num_tracked_points_left_hand = 21
-    num_tracked_points_right_hand = 21
+    num_tracked_points_body = len(mediapipe_body_landmark_names)
+    num_tracked_points_face = FACEMESH_NUM_LANDMARKS_WITH_IRISES
+    num_tracked_points_left_hand = len(mediapipe_hand_landmark_names)
+    num_tracked_points_right_hand = len(mediapipe_hand_landmark_names)
+    num_tracked_points_total = (
+        len(mediapipe_body_landmark_names)
+        + 2 * len(mediapipe_hand_landmark_names)
+        + FACEMESH_NUM_LANDMARKS_WITH_IRISES
+    )
     mediapipe_tracked_object_names = [
         "pose_landmarks",
         "face_landmarks",
