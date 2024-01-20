efdf35ee8fce1663d34c55b9e6602670b5123104
philipqueen <queen.philip@gmail.com>
2024-01-20 11:47:33 -0700

    fix runme duplication

--- RUN_ME.py
+++ RUN_ME.py
@@ -1,35 +1,66 @@
+from pathlib import Path
 import cv2
 
-from skellytracker.trackers.bright_point_tracker.brightest_point_tracker import BrightestPointTracker
+from skellytracker.trackers.bright_point_tracker.brightest_point_tracker import (
+    BrightestPointTracker,
+)
 from skellytracker.trackers.charuco_tracker.charuco_tracker import CharucoTracker
-from skellytracker.trackers.mediapipe_tracker.mediapipe_holistic_tracker import MediapipeHolisticTracker
-from skellytracker.trackers.segment_anything_tracker.segment_anything_tracker import SAMTracker
+from skellytracker.trackers.mediapipe_tracker.mediapipe_holistic_tracker import (
+    MediapipeHolisticTracker,
+)
+from skellytracker.trackers.segment_anything_tracker.segment_anything_tracker import (
+    SAMTracker,
+)
 from skellytracker.trackers.yolo_tracker.yolo_tracker import YOLOPoseTracker
-from skellytracker.trackers.yolo_object_tracker.yolo_object_tracker import YOLOObjectTracker
+from skellytracker.trackers.yolo_object_tracker.yolo_object_tracker import (
+    YOLOObjectTracker,
+)
+
+
+class DemoTypes:
+    WEBCAM = "webcam"
+    IMAGE = "image"
+
 
 if __name__ == "__main__":
-
     demo_tracker = "yolo_object_tracker"
-
+    demo_type = DemoTypes.WEBCAM
+    image_path = Path("/Path/To/Your/Image.jpg")
 
     if demo_tracker == "brightest_point_tracker":
-        BrightestPointTracker().demo()
+        tracker_object = BrightestPointTracker()
 
     elif demo_tracker == "charuco_tracker":
-        CharucoTracker(squaresX=7,
-                       squaresY=5,
-                       dictionary=cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)).demo()
+        tracker_object = CharucoTracker(
+            squaresX=7,
+            squaresY=5,
+            dictionary=cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250),
+        )
 
     elif demo_tracker == "mediapipe_holistic_tracker":
-        MediapipeHolisticTracker(model_complexity=2,
-                                 min_detection_confidence=0.5,
-                                 min_tracking_confidence=0.5,
-                                 static_image_mode=False,
-                                 smooth_landmarks=True).demo()
+        tracker_object = MediapipeHolisticTracker(
+            model_complexity=2,
+            min_detection_confidence=0.5,
+            min_tracking_confidence=0.5,
+            static_image_mode=False,
+            smooth_landmarks=True,
+        )
 
     elif demo_tracker == "yolo_tracker":
-        YOLOPoseTracker(model_size="high_res").demo()
+        tracker_object = YOLOPoseTracker(model_size="high_res")
+
     elif demo_tracker == "SAM_tracker":
-        SAMTracker().demo()
+        tracker_object = SAMTracker().demo()
+
     elif demo_tracker == "yolo_object_tracker":
-        YOLOObjectTracker(model_size="medium").demo()
+        tracker_object = YOLOObjectTracker(model_size="medium").demo()
+
+    else:
+        raise ValueError("Invalid demo tracker selection")
+
+    if demo_type == DemoTypes.WEBCAM:
+        tracker_object.demo()
+    elif demo_type == DemoTypes.IMAGE:
+        tracker_object.image_demo(image_path=image_path)
+    else:
+        tracker_object.demo()
