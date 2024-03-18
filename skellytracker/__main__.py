6043727958d8b1aa70ec77083984dd36752de881
philipqueen <queen.philip@gmail.com>
2024-03-18 12:49:44 -0600

    try again

--- skellytracker/__main__.py
+++ skellytracker/__main__.py
@@ -13,10 +13,13 @@ logger = logging.getLogger(__name__)
 from skellytracker.RUN_ME import main
 
 
-if __name__ == "__main__":
+def cli_main():
     logger.info(f"Running as a script")
     if len(sys.argv) > 1:
         demo_tracker = str(sys.argv[1])
     else:
         demo_tracker = "mediapipe_holistic_tracker"
     main(demo_tracker=demo_tracker)
+
+if __name__ == "__main__":
+    cli_main()
