36d7bde46f25e2872f578a8d0e1c747f6bcb00e6
philipqueen <queen.philip@gmail.com>
2024-03-18 11:30:17 -0600

    remove pyside from pyproject

--- skellytracker/__main__.py
+++ skellytracker/__main__.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__)
 from RUN_ME import main
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     logger.info(f"Running as a script")
     demo_tracker = "mediapipe_holistic_tracker"
     main(demo_tracker=demo_tracker)
