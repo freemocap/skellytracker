ebbb22e29c34e15cabd3b30c9b77134ff939e907
philipqueen <queen.philip@gmail.com>
2024-03-25 20:29:22 -0600

    fix parameter name

--- skellytracker/process_folder_of_videos.py
+++ skellytracker/process_folder_of_videos.py
@@ -153,7 +153,7 @@ def get_tracker(tracker_name: str, tracking_params: BaseModel) -> BaseTracker:
             min_detection_confidence=tracking_params.min_detection_confidence,
             min_tracking_confidence=tracking_params.min_tracking_confidence,
             static_image_mode=True,  # yolo cropping must be run with static image mode due to changing size of bounding boxes
-            bounding_box_buffer_percentage=tracking_params.yolo_bounding_box_buffer_percentage,
+            bounding_box_buffer_percentage=tracking_params.bounding_box_buffer_percentage,
             buffer_size_method=tracking_params.yolo_buffer_size_method,
         )
 
