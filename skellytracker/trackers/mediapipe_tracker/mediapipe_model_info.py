a63faaa67017ae00b5d45c9c12247ed56097e6de
philipqueen <queen.philip@gmail.com>
2024-04-09 19:19:32 -0400

    format, add back tracked object names

--- skellytracker/trackers/mediapipe_tracker/mediapipe_model_info.py
+++ skellytracker/trackers/mediapipe_tracker/mediapipe_model_info.py
@@ -9,15 +9,18 @@ from skellytracker.trackers.base_tracker.model_info import ModelInfo
 # https://imgur.com/a/aD74j
 # Winter, D.A. (2005) Biomechanics and Motor Control of Human Movement. 3rd Edition, John Wiley & Sons, Inc., Hoboken.
 class MediapipeModelInfo(ModelInfo):
-    body_landmark_names = [
+    landmark_names = [
         landmark.name.lower() for landmark in mp_holistic.PoseLandmark
     ]
-
-
-    num_tracked_points_body = len(body_landmark_names)
-
+    num_tracked_points = len(landmark_names)
+    tracked_object_names = [
+        "pose_landmarks",
+        "face_landmarks",
+        "left_hand_landmarks",
+        "right_hand_landmarks",
+    ]
     virtual_markers_definitions = {
-            "head_center": {
+        "head_center": {
             "marker_names": ["left_ear", "right_ear"],
             "marker_weights": [0.5, 0.5],
         },
@@ -26,208 +29,138 @@ class MediapipeModelInfo(ModelInfo):
             "marker_weights": [0.5, 0.5],
         },
         "trunk_center": {
-            "marker_names": ["left_shoulder", "right_shoulder", "left_hip", "right_hip"],
+            "marker_names": [
+                "left_shoulder",
+                "right_shoulder",
+                "left_hip",
+                "right_hip",
+            ],
             "marker_weights": [0.25, 0.25, 0.25, 0.25],
         },
         "hips_center": {
-            "marker_names": ["left_hip", "right_hip" ],
+            "marker_names": ["left_hip", "right_hip"],
             "marker_weights": [0.5, 0.5],
         },
     }
-
     segment_connections = {
-    "head":{
-        'proximal': 'left_ear',
-        'distal': 'right_ear'
-    },
-    "neck":{
-        'proximal': 'head_center',
-        'distal': 'neck_center',
-    },
-    "spine":{
-        'proximal': 'neck_center',
-        'distal': 'hips_center',
-    },
-    "right_shoulder":{
-        "proximal": "neck_center",
-        "distal": "right_shoulder"
-    },
-    "left_shoulder":{
-        "proximal": "neck_center",
-        "distal": "left_shoulder"
-    },
-    "right_upper_arm":{
-        "proximal": "right_shoulder",
-        "distal": "right_elbow"
-    },
-    "left_upper_arm":{
-        "proximal": "left_shoulder",
-        "distal": "left_elbow"
-    },
-    "right_forearm":{
-        "proximal": "right_elbow",
-        "distal": "right_wrist"
-    },
-    "left_forearm":{
-        "proximal": "left_elbow",
-        "distal": "left_wrist"
-    },
-    "right_hand":{
-        "proximal": "right_wrist",
-        "distal": "right_index"
-    },
-    "left_hand":{
-        "proximal": "left_wrist",
-        "distal": "left_index"
-    },
-    "right_pelvis": {
-        "proximal": "hips_center",
-        "distal": "right_hip"
-    },
-    "left_pelvis": {
-        "proximal": "hips_center",
-        "distal": "left_hip"
+        "head": {"proximal": "left_ear", "distal": "right_ear"},
+        "neck": {
+            "proximal": "head_center",
+            "distal": "neck_center",
         },
-    "right_thigh":{
-        "proximal": "right_hip",
-        "distal": "right_knee"
-    },
-    "left_thigh":{
-        "proximal": "left_hip",
-        "distal": "left_knee"
-    },
-    "right_shank":{
-        "proximal": "right_knee",
-        "distal": "right_ankle"
-    },
-    "left_shank":{
-        "proximal": "left_knee",
-        "distal": "left_ankle"
-    },
-    "right_foot":{
-        "proximal": "right_ankle",
-        "distal": "right_foot_index"
-    },
-    "left_foot":{
-        "proximal": "left_ankle",
-        "distal": "left_foot_index"
-    },
-    "right_heel":{
-        "proximal": "right_ankle",
-        "distal": "right_heel"
-    },
-    "left_heel":{
-        "proximal": "left_ankle",
-        "distal": "left_heel"
-    },
-    "right_foot_bottom":{
-        "proximal": "right_heel",
-        "distal": "right_foot_index"
-    },
-    "left_foot_bottom":{
-        "proximal": "left_heel",
-        "distal": "left_foot_index"
-    },
+        "spine": {
+            "proximal": "neck_center",
+            "distal": "hips_center",
+        },
+        "right_shoulder": {"proximal": "neck_center", "distal": "right_shoulder"},
+        "left_shoulder": {"proximal": "neck_center", "distal": "left_shoulder"},
+        "right_upper_arm": {"proximal": "right_shoulder", "distal": "right_elbow"},
+        "left_upper_arm": {"proximal": "left_shoulder", "distal": "left_elbow"},
+        "right_forearm": {"proximal": "right_elbow", "distal": "right_wrist"},
+        "left_forearm": {"proximal": "left_elbow", "distal": "left_wrist"},
+        "right_hand": {"proximal": "right_wrist", "distal": "right_index"},
+        "left_hand": {"proximal": "left_wrist", "distal": "left_index"},
+        "right_pelvis": {"proximal": "hips_center", "distal": "right_hip"},
+        "left_pelvis": {"proximal": "hips_center", "distal": "left_hip"},
+        "right_thigh": {"proximal": "right_hip", "distal": "right_knee"},
+        "left_thigh": {"proximal": "left_hip", "distal": "left_knee"},
+        "right_shank": {"proximal": "right_knee", "distal": "right_ankle"},
+        "left_shank": {"proximal": "left_knee", "distal": "left_ankle"},
+        "right_foot": {"proximal": "right_ankle", "distal": "right_foot_index"},
+        "left_foot": {"proximal": "left_ankle", "distal": "left_foot_index"},
+        "right_heel": {"proximal": "right_ankle", "distal": "right_heel"},
+        "left_heel": {"proximal": "left_ankle", "distal": "left_heel"},
+        "right_foot_bottom": {"proximal": "right_heel", "distal": "right_foot_index"},
+        "left_foot_bottom": {"proximal": "left_heel", "distal": "left_foot_index"},
     }
-
-    center_of_mass_anthropometric_data_table = {
-    "head": {
-        "segment_com_length": .5,
-        "segment_com_percentage": 0.081,
-    },
-    "spine": {
-        "segment_com_length": .5,
-        "segment_com_percentage": 0.497,
-    },
-    "right_upper_arm": {
-        "segment_com_length": .436,
-        "segment_com_percentage": 0.028,
-    },
-    "left_upper_arm": {
-        "segment_com_length": .436,
-        "segment_com_percentage": 0.028,
-    },
-    "right_forearm": {
-        "segment_com_length": .430,
-        "segment_com_percentage": 0.016,
-    },
-    "left_forearm": {
-        "segment_com_length": .430,
-        "segment_com_percentage": 0.016,
-    },
-    "right_hand": {
-        "segment_com_length": .506,
-        "segment_com_percentage": 0.006,
-    },
-    "left_hand": {
-        "segment_com_length": .506,
-        "segment_com_percentage": 0.006,
-    },
-    "right_thigh": {
-        "segment_com_length": .433,
-        "segment_com_percentage": 0.1,
-    },
-    "left_thigh": {
-        "segment_com_length": .433,
-        "segment_com_percentage": 0.1,
-    },
-    "right_shank": {
-        "segment_com_length": .433,
-        "segment_com_percentage": 0.0465,
-    },
-    "left_shank": {
-        "segment_com_length": .433,
-        "segment_com_percentage": 0.0465,
-    },
-    "right_foot": {
-        "segment_com_length": .5,
-        "segment_com_percentage": 0.0145,
-    },
-    "left_foot": {
-        "segment_com_length": .5,
-        "segment_com_percentage": 0.0145,
-    },
-}   
-
+    center_of_mass_definitions = {
+        "head": {
+            "segment_com_length": 0.5,
+            "segment_com_percentage": 0.081,
+        },
+        "spine": {
+            "segment_com_length": 0.5,
+            "segment_com_percentage": 0.497,
+        },
+        "right_upper_arm": {
+            "segment_com_length": 0.436,
+            "segment_com_percentage": 0.028,
+        },
+        "left_upper_arm": {
+            "segment_com_length": 0.436,
+            "segment_com_percentage": 0.028,
+        },
+        "right_forearm": {
+            "segment_com_length": 0.430,
+            "segment_com_percentage": 0.016,
+        },
+        "left_forearm": {
+            "segment_com_length": 0.430,
+            "segment_com_percentage": 0.016,
+        },
+        "right_hand": {
+            "segment_com_length": 0.506,
+            "segment_com_percentage": 0.006,
+        },
+        "left_hand": {
+            "segment_com_length": 0.506,
+            "segment_com_percentage": 0.006,
+        },
+        "right_thigh": {
+            "segment_com_length": 0.433,
+            "segment_com_percentage": 0.1,
+        },
+        "left_thigh": {
+            "segment_com_length": 0.433,
+            "segment_com_percentage": 0.1,
+        },
+        "right_shank": {
+            "segment_com_length": 0.433,
+            "segment_com_percentage": 0.0465,
+        },
+        "left_shank": {
+            "segment_com_length": 0.433,
+            "segment_com_percentage": 0.0465,
+        },
+        "right_foot": {
+            "segment_com_length": 0.5,
+            "segment_com_percentage": 0.0145,
+        },
+        "left_foot": {
+            "segment_com_length": 0.5,
+            "segment_com_percentage": 0.0145,
+        },
+    }
     joint_hierarchy = {
-        "hips_center": ["left_hip", 
-                    "right_hip", 
-                    "trunk_center"],
+        "hips_center": ["left_hip", "right_hip", "trunk_center"],
         "trunk_center": ["neck_center"],
-        "neck_center": ["left_shoulder", 
-                        "right_shoulder", 
-                        "head_center"],
-        "head_center": ["nose", 
-                        "left_eye_inner", 
-                        "left_eye", 
-                        "left_eye_outer", 
-                        "right_eye_inner", 
-                        "right_eye", 
-                        "right_eye_outer", 
-                        "left_ear", 
-                        "right_ear", 
-                        "mouth_left", 
-                        "mouth_right"],
+        "neck_center": ["left_shoulder", "right_shoulder", "head_center"],
+        "head_center": [
+            "nose",
+            "left_eye_inner",
+            "left_eye",
+            "left_eye_outer",
+            "right_eye_inner",
+            "right_eye",
+            "right_eye_outer",
+            "left_ear",
+            "right_ear",
+            "mouth_left",
+            "mouth_right",
+        ],
         "left_shoulder": ["left_elbow"],
         "left_elbow": ["left_wrist"],
-        "left_wrist": ["left_pinky",
-                        "left_index",
-                        "left_thumb"],
+        "left_wrist": ["left_pinky", "left_index", "left_thumb"],
         "right_shoulder": ["right_elbow"],
         "right_elbow": ["right_wrist"],
-        "right_wrist": ["right_pinky",
-                        "right_index",
-                        "right_thumb"],
+        "right_wrist": ["right_pinky", "right_index", "right_thumb"],
         "left_hip": ["left_knee"],
         "left_knee": ["left_ankle"],
-        "left_ankle": ["left_heel",
-                    "left_foot_index"],
+        "left_ankle": ["left_heel", "left_foot_index"],
         "right_hip": ["right_knee"],
         "right_knee": ["right_ankle"],
-        "right_ankle": ["right_heel",
-                        "right_foot_index"],
+        "right_ankle": ["right_heel", "right_foot_index"],
     }
-
 
 
 class MediapipeTrackingParams(BaseTrackingParams):
