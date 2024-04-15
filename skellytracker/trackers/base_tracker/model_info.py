0b12f19daeb691df209df5aa5ddfcfbd2f26dc1f
philipqueen <queen.philip@gmail.com>
2024-04-15 10:38:05 -0600

    remove optional flag and defaults from necessary modelinfo fields

--- skellytracker/trackers/base_tracker/model_info.py
+++ skellytracker/trackers/base_tracker/model_info.py
@@ -2,9 +2,9 @@ from typing import Dict, List, Optional
 
 
 class ModelInfo(dict):
-    landmark_names: Optional[List[str]] = None
-    num_tracked_points: Optional[int] = None
-    tracked_object_names: Optional[list] = None
+    landmark_names: List[str]
+    num_tracked_points: int
+    tracked_object_names: list
     virtual_markers_definitions: Optional[Dict[str, Dict[str, List[str | float]]]] = None
     segment_connections: Optional[Dict[str, Dict[str, str]]] = None
     center_of_mass_definitions: Optional[Dict[str, Dict[str, float]]] = None
