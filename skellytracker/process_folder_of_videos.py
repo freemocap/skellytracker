a02ab6cca3e9c09de36ca03b6b24c61dd2f11e36
philipqueen <queen.philip@gmail.com>
2024-04-17 13:28:31 -0600

    mess with path prefixes/suffixes

--- skellytracker/process_folder_of_videos.py
+++ skellytracker/process_folder_of_videos.py
@@ -25,11 +25,12 @@ from skellytracker.utilities.get_video_paths import get_video_paths
 
 logger = logging.getLogger(__name__)
 
+# TODO: figure out how we want to handle prefixes or suffixes here.
 file_name_dictionary = {
-    "MediapipeHolisticTracker": "mediapipe2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy",
-    "YOLOMediapipeComboTracker": "mediapipe2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy",
-    "YOLOPoseTracker": "yolo2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy",
-    "BrightestPointTracker": "brightestPoint2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy",
+    "MediapipeHolisticTracker": "2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy",
+    "YOLOMediapipeComboTracker": "2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy",
+    "YOLOPoseTracker": "2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy",
+    "BrightestPointTracker": "2dData_numCams_numFrames_numTrackedPoints_pixelXY.npy",
 }
 
 
@@ -115,7 +116,7 @@ def process_single_video(
     :return: Array of tracking data
     """
     video_name = (
-        video_path.stem + "_mediapipe.mp4"
+        video_path.stem + "_annotated.mp4"
     )  # TODO: fix it so blender output doesn't require mediapipe addendum here
     tracker = get_tracker(tracker_name=tracker_name, tracking_params=tracking_params)
     logger.info(
@@ -126,6 +127,10 @@ def process_single_video(
         output_video_filepath=annotated_video_path / video_name,
         save_data_bool=False,
     )
+
+    if output_array is None:
+        raise ValueError("Output array is None, verify that the tracker has an associated recorder")
+
     return output_array
 
 
