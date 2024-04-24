58187a261657e5c76407f097314863adb1770279
philipqueen <queen.philip@gmail.com>
2024-04-24 10:15:29 -0600

    make tracked object names optional in ModelInfo

--- skellytracker/trackers/base_tracker/model_info.py
+++ skellytracker/trackers/base_tracker/model_info.py
@@ -4,7 +4,7 @@ from typing import Dict, List, Optional
 class ModelInfo(dict):
     landmark_names: List[str]
     num_tracked_points: int
-    tracked_object_names: list
+    tracked_object_names: Optional[list] = None
     virtual_markers_definitions: Optional[Dict[str, Dict[str, List[str | float]]]] = None
     segment_connections: Optional[Dict[str, Dict[str, str]]] = None
     center_of_mass_definitions: Optional[Dict[str, Dict[str, float]]] = None
