e31d1b6d06f40426fc176b1980ab20502b30f8f1
philipqueen <queen.philip@gmail.com>
2024-06-25 17:57:28 -0600

    working multi person test

--- skellytracker/tests/test_yolo_pose_tracker.py
+++ skellytracker/tests/test_yolo_pose_tracker.py
@@ -92,7 +92,74 @@ def test_record(test_image):
             ]
         ]
     )
-    assert np.allclose(processed_results[:, :, :], expected_results[:, :, :], atol=1e-2)
+    assert np.allclose(processed_results, expected_results, atol=1e-2)
+
+
+# @pytest.mark.usefixtures("test_image")
+import cv2
+
+bus_image = cv2.imread("/Users/philipqueen/Downloads/bus.jpg")
+bus_image = cv2.cvtColor(bus_image, cv2.COLOR_BGR2RGB)
+
+
+def test_record_multiperson(test_image=bus_image):
+    max_det = 2
+    tracker = YOLOPoseTracker(model_size="nano", max_det=max_det)
+    tracked_objects = tracker.process_image(test_image)
+    tracker.recorder.record(tracked_objects=tracked_objects)
+    assert len(tracker.recorder.recorded_objects) == 1
+
+    processed_results = tracker.recorder.process_tracked_objects()
+    assert processed_results is not None
+    assert processed_results.shape == (
+        1,
+        (YOLOModelInfo.num_tracked_points * max_det),
+        2,
+    )
+
+    expected_results = np.array(
+        [
+            [
+                [142.94776916503906, 441.1304626464844],
+                [149.22552490234375, 432.66583251953125],
+                [132.95570373535156, 432.6253356933594],
+                [154.91513061523438, 438.90625],
+                [108.84307861328125, 438.0549621582031],
+                [169.82933044433594, 493.0794982910156],
+                [90.12742614746094, 494.21734619140625],
+                [193.97451782226562, 565.8185424804688],
+                [113.39808654785156, 563.470458984375],
+                [150.80001831054688, 564.606689453125],
+                [156.97048950195312, 556.6004028320312],
+                [159.6195831298828, 635.1295166015625],
+                [97.7007064819336, 636.2892456054688],
+                [182.3831024169922, 747.2435913085938],
+                [85.82090759277344, 754.9800415039062],
+                [187.85714721679688, 855.2320556640625],
+                [69.11174774169922, 866.8134765625],
+                [142.94776916503906, 441.1304626464844],
+                [149.22552490234375, 432.66583251953125],
+                [132.95570373535156, 432.6253356933594],
+                [154.91513061523438, 438.90625],
+                [108.84307861328125, 438.0549621582031],
+                [169.82933044433594, 493.0794982910156],
+                [90.12742614746094, 494.21734619140625],
+                [193.97451782226562, 565.8185424804688],
+                [113.39808654785156, 563.470458984375],
+                [150.80001831054688, 564.606689453125],
+                [156.97048950195312, 556.6004028320312],
+                [159.6195831298828, 635.1295166015625],
+                [97.7007064819336, 636.2892456054688],
+                [182.3831024169922, 747.2435913085938],
+                [85.82090759277344, 754.9800415039062],
+                [187.85714721679688, 855.2320556640625],
+                [69.11174774169922, 866.8134765625],
+            ]
+        ]
+    )
+    print(processed_results.tolist())
+    print(processed_results.shape)
+    assert np.allclose(processed_results, expected_results, atol=1e-2)
 
 
 class MockKeypoints:
@@ -111,6 +178,10 @@ def test_unpack_empty_results():
     tracked_person = tracker.tracked_objects["tracked_person_0"]
     assert tracked_person.pixel_x is None
     assert tracked_person.pixel_y is None
-    assert tracked_person.extra["landmarks"].shape == (1, YOLOModelInfo.num_tracked_points, 2)
+    assert tracked_person.extra["landmarks"].shape == (
+        1,
+        YOLOModelInfo.num_tracked_points,
+        2,
+    )
     assert np.isnan(tracked_person.extra["landmarks"][0, :, 0]).all()
     assert np.isnan(tracked_person.extra["landmarks"][0, :, 1]).all()
--- skellytracker/trackers/yolo_tracker/yolo_recorder.py
+++ skellytracker/trackers/yolo_tracker/yolo_recorder.py
@@ -18,12 +18,11 @@ class YOLORecorder(BaseRecorder):
         )
 
     def process_tracked_objects(self, **kwargs) -> np.ndarray:
-        num_tracked_points = YOLOModelInfo.num_tracked_points * len(self.recorded_objects[-1])
         recorded_object_arrays = []
         for i, recorded_object in enumerate(self.recorded_objects):
-            recorded_objects_arrays = []
+            frame_data_arrays = []
             for tracked_object in recorded_object:
-                tracked_object_data_array = np.zeros((len(self.recorded_objects), num_tracked_points, 2))
+                tracked_object_data_array = np.zeros((len(self.recorded_objects), YOLOModelInfo.num_tracked_points, 2))
                 for j in range(YOLOModelInfo.num_tracked_points):
                     tracked_object_data_array[i, j, 0] = tracked_object.extra[
                         "landmarks"
@@ -32,11 +31,13 @@ class YOLORecorder(BaseRecorder):
                         "landmarks"
                     ][0, j, 1]
 
-                recorded_objects_arrays.append(tracked_object_data_array)
+                frame_data_arrays.append(tracked_object_data_array)
 
-            recorded_object_data_array = np.concatenate(recorded_objects_arrays, axis=1)
+            recorded_object_data_array = np.concatenate(frame_data_arrays, axis=1)
+            print(f"shape of recorded_object_data_array: {recorded_object_data_array.shape}")
 
             recorded_object_arrays.append(recorded_object_data_array)
 
         self.recorded_objects_array = np.concatenate(recorded_object_arrays, axis=0)
+        print(f"shape of recorded_objects_array: {self.recorded_objects_array.shape}")
         return self.recorded_objects_array
