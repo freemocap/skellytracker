eaa11104a1e323c218a3e7fd2f66bbab08c8dad8
philipqueen <queen.philip@gmail.com>
2024-07-02 17:39:24 -0400

    support both v8 and v10, while setting 10 to default

--- skellytracker/trackers/yolo_object_tracker/yolo_object_model_info.py
+++ skellytracker/trackers/yolo_object_tracker/yolo_object_model_info.py
@@ -7,6 +7,11 @@ yolo_object_model_dictionary = {
     "medium": "yolov10m.pt",
     "large": "yolov10l.pt",
     "extra_large": "yolov10x.pt",
+    "nano8": "yolov8n.pt",
+    "small8": "yolov8s.pt",
+    "medium8": "yolov8m.pt",
+    "large8": "yolov8l.pt",
+    "extra_large8": "yolov8x.pt",
 }
 
 
