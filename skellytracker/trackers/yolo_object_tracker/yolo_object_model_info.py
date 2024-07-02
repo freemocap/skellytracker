251ce8a19b414c80849e50444441f2911b0a0dcf
philipqueen <queen.philip@gmail.com>
2024-07-02 17:32:42 -0400

    change model to v10

--- skellytracker/trackers/yolo_object_tracker/yolo_object_model_info.py
+++ skellytracker/trackers/yolo_object_tracker/yolo_object_model_info.py
@@ -2,11 +2,11 @@ from skellytracker.trackers.base_tracker.base_tracking_params import BaseTrackin
 
 
 yolo_object_model_dictionary = {
-    "nano": "yolov8n.pt",
-    "small": "yolov8s.pt",
-    "medium": "yolov8m.pt",
-    "large": "yolov8l.pt",
-    "extra_large": "yolov8x.pt",
+    "nano": "yolov10n.pt",
+    "small": "yolov10s.pt",
+    "medium": "yolov10m.pt",
+    "large": "yolov10l.pt",
+    "extra_large": "yolov10x.pt",
 }
 
 
