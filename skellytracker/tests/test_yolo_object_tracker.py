f0425be5dddd6ee906c203cb92bd4effe908bb33
philipqueen <queen.philip@gmail.com>
2024-07-03 18:57:05 -0400

    add model dictionary test, and update tests for new default model

--- skellytracker/tests/test_yolo_object_tracker.py
+++ skellytracker/tests/test_yolo_object_tracker.py
@@ -5,6 +5,9 @@ import numpy as np
 from skellytracker.trackers.yolo_object_tracker.yolo_object_tracker import (
     YOLOObjectTracker,
 )
+from skellytracker.trackers.yolo_object_tracker.yolo_object_model_info import (
+    yolo_object_model_dictionary,
+)
 
 
 @pytest.mark.usefixtures("test_image")
@@ -15,7 +18,7 @@ def test_process_image_person_only(test_image):
     assert len(tracked_objects) == 1
     assert tracked_objects["object"] is not None
     assert tracked_objects["object"].extra["boxes_xyxy"] is not None
-    assert np.allclose(tracked_objects["object"].extra["boxes_xyxy"], [90.676,96.981,493.54,812.03], atol=1e-2)
+    assert np.allclose(tracked_objects["object"].extra["boxes_xyxy"], [89.506,95.665,492.3,816.64], atol=1e-2)
     assert tracked_objects["object"].extra["original_image_shape"] == (1280, 720)
 
 @pytest.mark.usefixtures("test_image")
@@ -38,5 +41,19 @@ def test_record(test_image):
     assert processed_results.shape == (1,4)
 
     assert np.allclose(
-        processed_results, [90.676,96.981,493.54,812.03], atol=1e-2
+        processed_results, [89.506,95.665,492.3,816.64], atol=1e-2
     )
+
+def test_yolo_object_model_dictionary():
+    for key in yolo_object_model_dictionary.keys():
+        try:
+            YOLOObjectTracker(model_size=key)
+        except KeyError:
+            print(f"Invalid model size: {key}")
+        except FileNotFoundError:
+            print(f"Invalid model size (.pt file not found): {key}")
+
+    # try invalid model size
+    with pytest.raises(KeyError):
+        YOLOObjectTracker(model_size="invalid")
+
