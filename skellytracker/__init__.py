750f599efd1daeddc0d22c66e44962f57850989a
philipqueen <queen.philip@gmail.com>
2024-08-28 10:26:38 -0700

    Bump version v2024.06.1016 -> v2024.08.1017

--- skellytracker/__init__.py
+++ skellytracker/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for skellytracker"""
 
 __package_name__ = "skellytracker"
-__version__ = "v2024.06.1016"
+__version__ = "v2024.08.1017"
 
 __author__ = """Skelly FreeMoCap"""
 __email__ = "info@freemocap.org"
