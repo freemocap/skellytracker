df9b5b52db6264c15c810566fb51a949f69db3e2
jonmatthis <jonmatthis@gmail.com>
2024-12-29 16:59:39 -0500

    revert back to simpler detectBoard method

--- skellytracker/trackers/charuco_tracker/charuco_detector.py
+++ skellytracker/trackers/charuco_tracker/charuco_detector.py
@@ -5,9 +5,17 @@ import cv2
 import numpy as np
 
 from skellytracker.trackers.base_tracker.base_tracker import BaseDetectorConfig, BaseDetector
-from skellytracker.trackers.charuco_tracker.charuco_observations import CharucoObservation, CharucoObservationFactory
+from skellytracker.trackers.charuco_tracker.charuco_observations import CharucoObservation, CharucoObservationFactory, \
+    CharucoDetection
 
 DEFAULT_ARUCO_DICTIONARY = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
+
+
+class CharucoRefinementConfig:
+    window_size = (5, 5)
+    zero_zone = (-1, -1)
+    termination_criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+
 
 class CharucoDetectorConfig(BaseDetectorConfig):
     squares_x: int = 5
@@ -15,14 +23,11 @@ class CharucoDetectorConfig(BaseDetectorConfig):
     aruco_dictionary: cv2.aruco.Dictionary = DEFAULT_ARUCO_DICTIONARY
     square_length: float = 1
     marker_length: float = 0.8
+    refinement_config: CharucoRefinementConfig = CharucoRefinementConfig()
 
     @property
     def charuco_corner_ids(self) -> List[int]:
         return list(range((self.squares_x - 1) * (self.squares_y - 1)))
-
-    @property
-    def charuco_corner_names(self) -> List[str]:
-        return [f"CharucoCorner_{index}" for index in self.charuco_corner_ids]
 
 
 @dataclass
@@ -58,9 +63,46 @@ class CharucoDetector(BaseDetector):
         return list(self.board.getObjPoints())  # type: ignore
 
     def detect(self, image: np.ndarray) -> CharucoObservation:
+
+        result = self._get_detection_result(image)
+        if result is None:
+            return self.observation_factory.create_empty_observation()
+        return self.observation_factory.create_observation(result)
+
+    def _get_detection_result(self, image: np.ndarray) -> CharucoDetection | None:
         if len(image.shape) == 2:
             grey_image = image
         else:
             grey_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        result = self.detector.detectBoard(grey_image)
-        return self.observation_factory.create_observation(*result)
+
+        (raw_aruco_square_corners,
+         aruco_square_ids,
+         rejected_image_points) = cv2.aruco.detectMarkers(grey_image, self.config.aruco_dictionary)
+
+        if not raw_aruco_square_corners:
+            return
+
+        # refine detected corner locations to provide sub-pixel precision
+        # https://docs.opencv.org/4.x/dd/d1a/group__imgproc__feature.html#ga354e0d7c86d0d9da75de9b9701a9a87e
+        refined_aruco_corners = tuple(cv2.cornerSubPix(grey_image,
+                                                       corner,
+                                                       winSize=self.config.refinement_config.window_size,
+                                                       zeroZone=self.config.refinement_config.zero_zone,
+                                                       criteria=self.config.refinement_config.termination_criteria) for
+                                      corner in raw_aruco_square_corners)
+
+        (refinement_success,
+         charuco_corners,
+         charuco_ids) = cv2.aruco.interpolateCornersCharuco(tuple(refined_aruco_corners),
+                                                            aruco_square_ids,
+                                                            grey_image,
+                                                            self.board,
+                                                            )
+
+
+        return CharucoDetection(charuco_corner_ids=charuco_ids,
+                                charuco_corners=charuco_corners,
+                                aruco_marker_ids=aruco_square_ids,
+                                aruco_marker_corners=refined_aruco_corners,
+                                raw_aruco_corners=raw_aruco_square_corners,
+                                rejected_image_points=rejected_image_points)
--- skellytracker/trackers/charuco_tracker/charuco_observations.py
+++ skellytracker/trackers/charuco_tracker/charuco_observations.py
@@ -3,7 +3,7 @@ from typing import List
 
 import numpy as np
 
-from skellytracker.trackers.base_tracker.base_tracker import BaseObservation, BaseObservationFactory
+from skellytracker.trackers.base_tracker.base_tracker import BaseObservation, BaseObservationFactory, BaseDetection
 
 
 @dataclass
@@ -11,8 +11,30 @@ class CharucoObservation(BaseObservation):
     charuco_corners: dict[int, tuple[float, float] | None]
     aruco_marker_corners: dict[int, list[tuple[float, float]] | None]
 
+    def create_empty_observation(self):
+        return CharucoObservation(
+            charuco_corners={},
+            aruco_marker_corners={},
+        )
+
 
 CharucoObservations = list[CharucoObservation]
+
+
+@dataclass
+class CharucoDetection(BaseDetection):
+    """
+    Output of the CharucoTracker.detect() method, to be re-shaped into a CharucoObservation.
+    """
+    aruco_marker_ids: list[list[int]]
+    aruco_marker_corners: tuple[np.ndarray]
+    raw_aruco_corners: np.ndarray
+    rejected_image_points: np.ndarray
+
+    charuco_corners: np.ndarray|None = None
+    charuco_corner_ids: list[list[int]]|None = None
+
+
 
 
 @dataclass
@@ -21,43 +43,47 @@ class CharucoObservationFactory(BaseObservationFactory):
     aruco_marker_ids: List[int]
 
     def create_observation(self,
-                           detected_charuco_corners: np.ndarray,
-                           detected_charuco_corner_ids: list[list[int]],
-                           detected_aruco_marker_corners: np.ndarray,
-                           detected_aruco_marker_ids: list[list[int]],
-                           ) -> CharucoObservation:
+                           detection: CharucoDetection) -> CharucoObservation:
+
         charuco_corners_out = {id: None for id in self.charuco_corner_ids}
         aruco_marker_corners_out = {id: None for id in self.aruco_marker_ids}
 
-        if detected_charuco_corner_ids is None or len(detected_charuco_corner_ids) == 0:
-            detected_charuco_corner_ids = None
-            detected_charuco_corners = None
-        elif len(detected_charuco_corner_ids) == 1:
-            detected_charuco_corner_ids = list(detected_charuco_corner_ids[0])
-            detected_charuco_corners = list(detected_charuco_corners[0])
-        elif len(detected_charuco_corner_ids) > 1:
-            detected_charuco_corner_ids = list(np.squeeze(detected_charuco_corner_ids))
-            detected_charuco_corners = list(np.squeeze(detected_charuco_corners))
+        # TODO - Move this weird conversion logic to the `Detection` class?
+        if detection.charuco_corner_ids is None or len(detection.charuco_corner_ids) == 0:
+            detection.charuco_corner_ids = None
+            detection.charuco_corners = None
+        elif len(detection.charuco_corner_ids) == 1:
+            detection.charuco_corner_ids = list(detection.charuco_corner_ids[0])
+            detection.charuco_corners = list(detection.charuco_corners[0])
+        elif len(detection.charuco_corner_ids) > 1:
+            detection.charuco_corner_ids = list(np.squeeze(detection.charuco_corner_ids))
+            detection.charuco_corners = list(np.squeeze(detection.charuco_corners))
 
-        if detected_aruco_marker_ids is None or len(detected_aruco_marker_ids) == 0:
-            detected_aruco_marker_ids = None
-            detected_aruco_marker_corners = None
-        elif len(detected_aruco_marker_ids) == 1:
-            detected_aruco_marker_ids = list(detected_aruco_marker_ids[0])
-            detected_aruco_marker_corners = list(detected_aruco_marker_corners[0])
-        elif len(detected_aruco_marker_ids) > 1:
-            detected_aruco_marker_ids = list(np.squeeze(detected_aruco_marker_ids))
-            detected_aruco_marker_corners = list(np.squeeze(detected_aruco_marker_corners))
+        if detection.aruco_marker_ids is None or len(detection.aruco_marker_ids) == 0:
+            detection.aruco_marker_ids = None
+            detection.aruco_marker_corners = None
+        elif len(detection.aruco_marker_ids) == 1:
+            detection.aruco_marker_ids = list(detection.aruco_marker_ids[0])
+            detection.aruco_marker_corners = list(detection.aruco_marker_corners[0])
+        elif len(detection.aruco_marker_ids) > 1:
+            detection.aruco_marker_ids = list(np.squeeze(detection.aruco_marker_ids))
+            detection.aruco_marker_corners = list(np.squeeze(detection.aruco_marker_corners))
 
-        if detected_charuco_corner_ids is not None:
-            for corner_id, corner in zip(detected_charuco_corner_ids, detected_charuco_corners):
+        if detection.charuco_corner_ids is not None:
+            for corner_id, corner in zip(detection.charuco_corner_ids, detection.charuco_corners):
                 charuco_corners_out[corner_id] = corner
 
-        if detected_aruco_marker_ids is not None:
-            for marker_id, corner in zip(detected_aruco_marker_ids, detected_aruco_marker_corners):
+        if detection.aruco_marker_ids is not None:
+            for marker_id, corner in zip(detection.aruco_marker_ids, detection.aruco_marker_corners):
                 aruco_marker_corners_out[marker_id] = corner
 
         return CharucoObservation(
             charuco_corners=charuco_corners_out,
             aruco_marker_corners=aruco_marker_corners_out,
+        )
+
+    def create_empty_observation(self) -> CharucoObservation:
+        return CharucoObservation(
+            charuco_corners={id: None for id in self.charuco_corner_ids},
+            aruco_marker_corners={id: None for id in self.aruco_marker_ids},
         )
