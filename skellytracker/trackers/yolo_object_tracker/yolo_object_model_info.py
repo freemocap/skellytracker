6f2fa64f5d6a39b6f9123be0463cd2a8e5382371
Aaron Cherian <aaron.cherian2@gmail.com>
2025-05-12 11:45:15 -0400

    try to upgrade yolo to v11

--- skellytracker/trackers/yolo_object_tracker/yolo_object_model_info.py
+++ skellytracker/trackers/yolo_object_tracker/yolo_object_model_info.py
@@ -2,11 +2,11 @@ from skellytracker.trackers.base_tracker.base_tracking_params import BaseTrackin
 
 
 yolo_object_model_dictionary = {
-    "nano": "yolov8n.pt",
-    "small": "yolov8s.pt",
-    "medium": "yolov8m.pt",
-    "large": "yolov8l.pt",
-    "extra_large": "yolov8x.pt",
+    "nano": "yolov11n.pt",
+    "small": "yolov11s.pt",
+    "medium": "yolov11m.pt",
+    "large": "yolov11l.pt",
+    "extra_large": "yolov11x.pt",
 }
 
 
