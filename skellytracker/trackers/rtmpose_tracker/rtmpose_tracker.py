8a6f0ee30a2687a74336feaee59ce51e035f2c98
philipqueen <queen.philip@gmail.com>
2025-05-21 16:47:34 -0600

    Changes to RTM Pose (#58)

--- skellytracker/trackers/rtmpose_tracker/rtmpose_tracker.py
+++ skellytracker/trackers/rtmpose_tracker/rtmpose_tracker.py
@@ -4,12 +4,16 @@ from skellytracker.trackers.rtmpose_tracker.rtmpose_recorder import RTMPoseRecor
 from skellytracker.trackers.base_tracker.tracked_object import TrackedObject
 from rtmlib import Wholebody, draw_skeleton
 import numpy as np
+import platform
 from typing import Dict
+import logging
 
 
 
 from pathlib import Path
 import onnxruntime
+
+logger = logging.getLogger(__name__)
 
 class RTMPoseTracker(BaseTracker):
         def __init__(
@@ -20,35 +24,35 @@ class RTMPoseTracker(BaseTracker):
                 recorder = RTMPoseRecorder(),
                 )
 
-                device = "cuda"
+                device = self._get_device()
+                
                 self.wholebody_model = Wholebody(
                 to_openpose = False,
                 backend ='onnxruntime',
                 device = device
                 )
 
-
-                onnx_path = Path(self.wholebody_model.pose_model.onnx_model)
-                providers = ["CUDAExecutionProvider"] if device == device else ["CPUExecutionProvider"]
-                self.pose_session = onnxruntime.InferenceSession(str(onnx_path), providers=providers)
-                f = 2
-        
-        def process_image(self, image: np.ndarray, **kwargs) -> Dict[str, TrackedObject]:
-                slices = {"body_slice": slice(0, 23),
+                self.slices = {"body_slice": slice(0, 23),
                         "face_slice": slice(23, 91),
                         "left_hand_slice": slice(91, 112),
                         "right_hand_slice": slice(112, 133)}
 
-                keypoints, scores = self.wholebody_model(image)
-                if keypoints.ndim == 3: #if multiple people are detected
-                        results = np.array(keypoints[0])     # pick the primary person
+                # if device == "cuda":
+                #         onnx_path = Path(self.wholebody_model.pose_model.onnx_model)
+                #         providers = ["CUDAExecutionProvider"]
+                #         self.pose_session = onnxruntime.InferenceSession(str(onnx_path), providers=providers)
+                # f = 2
 
-                else:
-                        results = keypoints
-                self.tracked_objects["pose_landmarks"].extra["landmarks"] = results[slices["body_slice"]]
-                self.tracked_objects["face_landmarks"].extra["landmarks"] = results[slices["face_slice"]]
-                self.tracked_objects["left_hand_landmarks"].extra["landmarks"] = results[slices["left_hand_slice"]]
-                self.tracked_objects["right_hand_landmarks"].extra["landmarks"] = results[slices["right_hand_slice"]]
+        def process_image(self, image: np.ndarray, **kwargs) -> Dict[str, TrackedObject]:
+                keypoints, scores = self.wholebody_model(image)
+
+                # pick the primary person if multiple people are detected
+                results = np.array(keypoints[0]) if keypoints.ndim == 3 else keypoints 
+
+                self.tracked_objects["pose_landmarks"].extra["landmarks"] = results[self.slices["body_slice"]]
+                self.tracked_objects["face_landmarks"].extra["landmarks"] = results[self.slices["face_slice"]]
+                self.tracked_objects["left_hand_landmarks"].extra["landmarks"] = results[self.slices["left_hand_slice"]]
+                self.tracked_objects["right_hand_landmarks"].extra["landmarks"] = results[self.slices["right_hand_slice"]]
 
                 self.annotated_image = self.annotate_image(
                                         image=image,
@@ -62,6 +66,18 @@ class RTMPoseTracker(BaseTracker):
                                         openpose_skeleton=False,
                                         kpt_thr=0.5)
                 return image_annotated
+        
+        def _get_device(self):
+            if platform.system() == "Darwin":
+                    device = "mps"
+                    logger.debug("MacOS detected, attempting to use MPS")
+            elif onnxruntime.get_device() == "GPU":
+                    device = "cuda"
+                    logger.debug("onnxruntime-gpu installed, attempting to use CUDA")
+            else:
+                    device = "cpu"
+                    logger.debug("onnxruntime (non gpu version) installed, using CPU")
+            return device
 
 if __name__ == "__main__":
     RTMPoseTracker().demo()
