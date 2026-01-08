494b8ba4d716e1fd9b73eabc281081d6d6afccaf
Aaron Cherian <aaron.cherian2@gmail.com>
2026-01-08 18:30:00 -0500

    Create vitpose_detector.py

---
+++ skellytracker/trackers/vitpose_tracker/vitpose_detector.py
@@ -0,0 +1,25 @@
+from skellytracker.trackers.base_tracker.base_tracker_abcs import BaseDetector, BaseDetectorConfig
+from easy_ViTPose import VitInference
+
+
+class VITPoseDetectorConfig(BaseDetectorConfig):
+    confidence_threshold: float = 0.5
+    model: str = "base" #options are 'small', 'base', 'large', and 'huge'
+    device: str|None = None 
+    #later can add a bunch of other options, such as YOLO size, YOLO step size, YOLO bb size
+
+class VITPoseDetector(BaseDetector):
+    config: VITPoseDetectorConfig
+    detector: VitInference
+
+    @classmethod
+    def create(cls, config: VITPoseDetectorConfig | None = None):
+        if config is None:
+            config = VITPoseDetectorConfig()
+        return cls(config=config)
+    
+    @classmethod
+    def create(cls, config: VITPoseDetectorConfig | None = None):
+        detector = VitInference()
+
+    def _resolve_model_type(self, model)
