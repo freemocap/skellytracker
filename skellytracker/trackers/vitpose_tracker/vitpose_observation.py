2b633a59ad2c8da92d41b198acf7daa99af58d0e
Aaron Cherian <aaron.cherian2@gmail.com>
2026-01-12 11:07:32 -0500

    Update vitpose_observation.py

--- skellytracker/trackers/vitpose_tracker/vitpose_observation.py
+++ skellytracker/trackers/vitpose_tracker/vitpose_observation.py
@@ -13,7 +13,7 @@ class VITPoseObservation(BaseObservation):
         # Handle no detections
         if len(results) == 0:
             # Return NaN-filled array - 133 keypoints for wholebody (this number is unlikely to change, unless we also allow for other kinds of VIT models. At that point we'll need to account for that)
-            num_keypoints = 25
+            num_keypoints = 133
             keypoints = np.full((num_keypoints, 3), np.nan, dtype=np.float32)
         else:
             keypoints = results[0]  # First person only
